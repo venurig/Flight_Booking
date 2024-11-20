6c8e91cc6b9b9e0b70bb3f25deb7f72ba1536144
V E N U R I <V E N U R I@172.16.102.66>
2024-11-20 10:11:03 +0530

    First Commit!

---
+++ src/flight_booking_system/Flight_Booking_System.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package flight_booking_system;
+
+/**
+ *
+ * @author V E N U R I
+ */
+public class Flight_Booking_System {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
